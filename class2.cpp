4576c0b5532282db7ac638f919869957f6629cb1
KhoirulArifPratama <khoirularif932@gmail.com>
2025-04-28 11:21:36 +0700

    menambahkan public persegi panjang

--- class2.cpp
+++ class2.cpp
@@ -7,3 +7,14 @@ private:
     int panjang;
     int lebar;
     int luas;
+
+public:
+    void inputdata()
+    {
+        cout << "Masukkan panjang = ";
+        cin >> panjang;
+        cout << "Masukkan lebar = ";
+        cin >> lebar;
+        cout << "Masukkan luas = ";
+        cin >> luas;
+    }
